59b2a2271b8f1ab8a26ee1b42d3472f1e20ea0c1
Topiltzin <nokoayzack@gmail.com>
2019-08-13 18:11:38 -0500

    Added a constant file for the routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,12 @@ import React, { Fragment } from 'react'
 import { Switch, Route } from 'react-router-dom'
 // import PrivateRoute from './privateRoute'
 import Landing from '../components/landing'
+import { ROOT } from '../utils/routes';
 
 const Routes = () =>
     <Fragment>
         <Switch>
-            <Route path="/" component={ Landing } />
+            <Route path={ROOT} component={ Landing } />
         </Switch>
     </Fragment>
 

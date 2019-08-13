aa0979b208f2ae6faf02b57ea7ae3f556a605218
Topiltzin <nokoayzack@gmail.com>
2019-08-13 18:16:05 -0500

    Changed the structure of the sites

--- src/components/Sites/landing/index.jsx
+++ src/components/Sites/landing/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Landing = () => {
     return (
         <React.Fragment>
             <h1>Welcome to México Trasciende</h1>
         </React.Fragment>
-    )
-}
+    );
+};
 
 export default Landing

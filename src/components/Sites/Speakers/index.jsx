709889c9cde0013cc9a49abd4d36f58b1e4e9d29
Topiltzin <nokoayzack@gmail.com>
2019-08-15 17:20:37 -0500

    SpeakerCard now activates the scroll animation

--- src/components/Sites/Speakers/index.jsx
+++ src/components/Sites/Speakers/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styles from './styles.module.scss'
 
 /**
@@ -9,12 +9,16 @@ import styles from './styles.module.scss'
  * @param {string} props.avatar
  * @param {string} props.bio
  */
-const SpeakerCard = ({ name, avatar, bio, ...props }) => {
+const SpeakerCard = ({ name, avatar, bio, y, visibility, setVisible, id, ...props }) => {
     const [state, setState] = useState('normal')
+
+    const ref = useRef()
 
     const handleClick = () => {
         if(state === 'normal') {
+            window.scrollTo(0, ref.current.offsetTop - window.innerHeight / 2)
             setState('focus')
+            setVisible({id, ref })
         } else if(state === 'focus') {
             setState('opened')
         } else if(state === 'opened') {
@@ -22,10 +26,33 @@ const SpeakerCard = ({ name, avatar, bio, ...props }) => {
         }
     }
 
+    useEffect(() => {
+        if(visibility) {
+            setState(visibility)
+        }
+
+        const middle = ref.current.offsetTop - window.innerHeight / 2
+        const top = middle - 35
+        const bot = middle + 35
+
+        // console.log('*******ID', id)
+        // console.log('middle', middle, 'top', top, 'bot', bot)
+        // console.log('Y', parseInt(y), )
+        // console.log('calc', y <= bot && y >= top)
+        if(y <= bot && y >= top) {
+            if(state === 'normal') {
+                window.scrollTo(0, ref.current.offsetTop - window.innerHeight / 2)
+                setState('focus')
+                setVisible({id, ref})
+            }
+        }
+    }, [visibility, y])
+
     return(
         <div {...props}
             className={[styles.SpeakerCard, styles[state]].join(' ')}
-            onClick={handleClick} >
+            onClick={handleClick}
+            ref={ref} >
             <div className={styles.focusArea} >
                 <div className={styles.imgCont} >
                     <img src={avatar} />
@@ -75,15 +102,28 @@ const Speakers = () => {
         }
     ]
 
+    const [ y, setY ] = useState(0)
+    const [visibile, setVisible] = useState({id: -1, ref: null})
+
+    const handleScroll = () => {
+        setY(window.scrollY)
+        console.log(window.scrollY)
+    }
+
+    useEffect(() => {
+        return window.addEventListener('scroll', handleScroll)
+    }, [])
+
     return(
         <div className={styles.Speakers} >
+            {/* onScroll={handleScroll} > */}
             <div className={styles.header} >
                 <img src="http://www.ihsig.com/media-intestinal-health-ihsig/ihsig767-1200x1200mm-1024x505.png" />
             </div>
 
             {
                 speakers.map((speaker, i) =>
-                    <SpeakerCard key={i} {...speaker} />
+                    <SpeakerCard setVisible={setVisible} visibility={visibile.id === i ? 'focus' : 'normal'} key={i} y={y} id={i} {...speaker} />
                 )
             }
         </div>

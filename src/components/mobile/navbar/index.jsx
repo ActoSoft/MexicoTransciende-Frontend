6a19956721de7bf006dae9c261aa59f3e7acd69d
UlisesGomez <ulisesgomez08live@gmail.com>
2019-08-20 00:08:45 -0500

    Responsive Text

--- src/components/mobile/navbar/index.jsx
+++ src/components/mobile/navbar/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './index.css'
+import './index.scss'
 import Icon from './../../../images/logo-transparency.png'
 import User from './../../../images/user.svg'
 class Navbar extends Component {
@@ -10,19 +10,19 @@ class Navbar extends Component {
         }
     }
 
-	render() {
+    render() {
 	    return (
 	        <div className="ui-navbar">
-            <div className="ui-hamburger-menu">
-                <div className="line"></div>
-                <div className="line-large"></div>
-                <div className="line"></div>
-            </div>
-            <img src={Icon} alt="User" className="img-icon"/>
-            <img src={User} alt="User" className="img-nav"/>
+                <div className="ui-hamburger-menu">
+                    <div className="line"></div>
+                    <div className="line-large"></div>
+                    <div className="line"></div>
+                </div>
+                <img src={Icon} alt="User" className="img-icon"/>
+                <img src={User} alt="User" className="img-nav"/>
             </div>
 	    )
-	}
+    }
 }
 
 export default Navbar 

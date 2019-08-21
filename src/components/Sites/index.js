8130b6ab7ccefd2030856e6e3c9f4d7f847ed88d
UlisesGomez <ulisesgomez08live@gmail.com>
2019-08-21 17:15:08 -0500

    InfoEvent

--- src/components/Sites/index.js
+++ src/components/Sites/index.js
@@ -2,4 +2,5 @@ import Landing from './landing'
 import Speakers from './Speakers'
 import AboutUs from './AboutUs'
 import NavbarSidebar from './Navbar'
-export { Landing, Speakers, AboutUs, NavbarSidebar }
+import InfoEvent from './InfoEvent'
+export { Landing, Speakers, AboutUs, NavbarSidebar, InfoEvent }

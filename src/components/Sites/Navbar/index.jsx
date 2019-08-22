b1a78f5c5eb186f86a4c875561899d335bf536e1
Martin Melo <nitram-210397@hotmail.com>
2019-08-22 00:43:03 -0500

    Background with red opacity and white logo

--- src/components/Sites/Navbar/index.jsx
+++ src/components/Sites/Navbar/index.jsx
@@ -1,41 +1,77 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, Component } from 'react'
 import logo from '../../../images/logo-transparency-square.png'
 import cross from '../../../images/icons/multiply.svg'
 import menu from '../../../images/icons/menu.svg'
 import user from '../../../images/icons/user.svg'
 
-const NavbarSidebar = () => {
+class NavbarSidebar extends Component {
 
-    const [openSidebar, changeSidebar] = useState(false)
+    constructor(props) {
+        super(props)
+        this.state = {
+            sidebarOpen: false,
+            navbarBackground: false
+        }
+    }
 
-    return(
-        <Fragment>
-            <div className = { `App ${openSidebar ? 'right-move' : 'left-move'}` }>
-                <div className = { `side-bar ${openSidebar ? 'side-visible' : 'side-hidde'}` }>
-                    <img src = { cross } alt = '' className = 'cross'
-                        onClick = { () => changeSidebar(false) }
-                    />
-                    <ul className = 'options-container' >
-                        <li className = 'link' onClick = { () => changeSidebar(false) }>Inicio</li>
-                        <li className = 'link' onClick = { () => changeSidebar(false) }>Acerca de</li>
-                        <li className = 'link' onClick = { () => changeSidebar(false) }>Speakers</li>
-                        <li className = 'link' onClick = { () => changeSidebar(false) }>Equipo Org.</li>
-                        <li className = 'link' onClick = { () => changeSidebar(false) }>Patrocinadores</li>
-                        <li className = 'link' onClick = { () => changeSidebar(false) }>Contacto</li>
-                    </ul>
-                    <img src = { logo } alt = '' className = 'logo-sidebar'/>
-                </div>
-                <div className = 'the-other-content'>
-                    <header>
-                        <img src = { menu } alt = ''
-                            onClick = { () => changeSidebar(true) }
+    changePosition = () => {
+        let { scrollY, innerHeight } = window
+        let { navbarBackground } = this.state
+        if(scrollY > innerHeight && !navbarBackground) {
+            this.setState({ navbarBackground: true })
+        }
+
+        if(scrollY < innerHeight && navbarBackground) {
+            this.setState({ navbarBackground: false })
+        }
+    }
+
+    componentDidMount = () => {
+        window.addEventListener('scroll', this.changePosition)
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener('scroll', this.changePosition)
+    }
+
+    changeSidebar = value => {
+        this.setState({ sidebarOpen: value })
+    }
+
+    render() {
+        return(
+            <Fragment>
+                <div className = { `App ${this.state.sidebarOpen ? 'right-move' : 'left-move'}` }>
+                    <div className = { `side-bar ${this.state.sidebarOpen ? 'side-visible' : 'side-hidde'}` }>
+                        <img src = { cross } alt = '' className = 'cross'
+                            onClick = { () => this.changeSidebar(false) }
                         />
-                        <img src = { logo } alt = ''/>
-                        <img src = { user } alt = ''/>
-                    </header>
+                        <ul className = 'options-container' >
+                            <li className = 'link' onClick = { () => this.changeSidebar(false) }>Inicio</li>
+                            <li className = 'link' onClick = { () => this.changeSidebar(false) }>Acerca de</li>
+                            <li className = 'link' onClick = { () => this.changeSidebar(false) }>Speakers</li>
+                            <li className = 'link' onClick = { () => this.changeSidebar(false) }>Equipo Org.</li>
+                            <li className = 'link' onClick = { () => this.changeSidebar(false) }>Patrocinadores</li>
+                            <li className = 'link' onClick = { () => this.changeSidebar(false) }>Contacto</li>
+                        </ul>
+                        <img src = { logo } alt = '' className = 'logo-sidebar'/>
+                    </div>
+                    <div className = 'the-other-content'>
+                        <header
+                            style = {{
+                                backgroundColor: this.state.navbarBackground ? '#fff' : 'transparent'
+                            }}
+                        >
+                            <img src = { menu } alt = ''
+                                onClick = { () => this.changeSidebar(true) }
+                            />
+                            <img src = { logo } alt = ''/>
+                            <img src = { user } alt = ''/>
+                        </header>
+                    </div>
                 </div>
-            </div>
-        </Fragment>
-    )
+            </Fragment>
+        )
+    }
 }
 export default NavbarSidebar
--- src/components/Sites/landing/index.jsx
+++ src/components/Sites/landing/index.jsx
@@ -1,9 +1,28 @@
 import React, { Fragment } from 'react'
+import './index.scss'
+import whiteLogo from '../../../images/logo-white-color-transparency.png'
+
+const images = [
+    'https://images.pexels.com/photos/1089842/pexels-photo-1089842.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+]
 
 const Landing = () => {
     return (
         <Fragment>
-            <h1>Welcome to México Trasciende</h1>
+            <div
+                className = 'slider-container'
+                style = {{
+                    backgroundImage: `linear-gradient(rgba(255, 0, 0, 0.30),
+                    rgba(255, 0, 0, 0.30)),
+                    url('${images[0]}'`
+                }}
+            >
+                <img
+                    src = { whiteLogo }
+                    className = 'logo-cover'
+                    alt = 'MXT Logo'
+                />
+            </div>
         </Fragment>
     )
 }

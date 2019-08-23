95331b5dbba900847234b0e416a111bea4903023
Martin Melo <nitram-210397@hotmail.com>
2019-08-23 02:06:16 -0500

    Sidebar less

--- src/components/Sites/landing/index.jsx
+++ src/components/Sites/landing/index.jsx
@@ -1,33 +1,60 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import './index.scss'
 import whiteLogo from '../../../images/logo-white-color-transparency.png'
 import CountDown from './Countdown'
 
-const images = [
-    'https://images.pexels.com/photos/1089842/pexels-photo-1089842.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-]
 
-const Landing = () => {
-    return (
-        <Fragment>
-            <div
-                className = 'slider-container'
-                style = {{
-                    backgroundImage: `linear-gradient(rgba(255, 0, 0, 0.30),
-                    rgba(255, 0, 0, 0.30)),
-                    url('${images[0]}'`
-                }}
-            >
-                <img
-                    src = { whiteLogo }
-                    className = 'logo-cover'
-                    alt = 'MXT Logo'
-                />
+class Landing extends Component {
 
-                <CountDown />
-            </div>
-        </Fragment>
-    )
+    constructor(props) {
+        super(props)
+        this.state = {
+            images: [],
+            actualImage: 0
+        }
+    }
+
+    getImages = () => {
+        this.setState({ images: [
+            'https://images.pexels.com/photos/1089842/pexels-photo-1089842.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            'https://images.pexels.com/photos/1181395/pexels-photo-1181395.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            'https://images.pexels.com/photos/32976/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        ]})
+    }
+    componentDidMount = () => {
+        this.getImages()
+        setInterval(() => {
+            let { images, actualImage } = this.state
+            actualImage = actualImage + 1 === images.length
+                ? 0 : actualImage + 1
+            this.setState({ actualImage })
+        }, 5000)
+    }
+
+
+    render() {
+        const { images, actualImage } = this.state
+        return (
+            <Fragment>
+                <div
+                    className = 'slider-container'
+                    style = {{
+                        backgroundImage: `linear-gradient(rgba(255, 0, 0, 0.30),
+                        rgba(176, 59, 60, 0.30)),
+                        url('${images[actualImage]}'`
+                    }}
+                >
+                    <img
+                        src = { whiteLogo }
+                        className = 'logo-cover'
+                        alt = 'MXT Logo'
+                    />
+
+                    <CountDown />
+                </div>
+            </Fragment>
+        )
+    }
 }
 
 export default Landing

3b3e05823e399d746aaa684b64e8272cffc8544c
Martin Melo <nitram-210397@hotmail.com>
2019-09-09 02:02:03 -0500

    navbar on admin panel

--- src/components/Sites/Admin/Dashboard/index.jsx
+++ src/components/Sites/Admin/Dashboard/index.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 import { AdminRoutes } from '../../../../routes'
+import { NavLink } from 'react-router-dom'
+import { DASHBOARD, ASSISTANTS, REGISTER } from '../../../../utils/routes'
 import './index.scss'
-const { Content } = Layout
-
+const { Content, Header } = Layout
+const { Item } = Menu
 const Dashboard = () => {
     return (
         <Layout className = 'layout'>
+            <Header className = 'header'>
+                <Menu
+                    theme = 'light'
+                    mode = 'horizontal'
+                    defaultSelectedKeys = { ['dashboard'] }
+                    style = {{ lineHeight: '64px' }}
+                >
+                    <Item key = 'dashboard'>
+                        <NavLink to = { DASHBOARD }>
+                            Inicio
+                        </NavLink>
+                    </Item>
+                    <Item key = 'assistants'>
+                        <NavLink to = { DASHBOARD + ASSISTANTS }>
+                            Ver asistentes
+                        </NavLink>
+                    </Item>
+                    <Item key = 'register'>
+                        <NavLink to = { DASHBOARD + REGISTER }>
+                            Registrar asistente
+                        </NavLink>
+                    </Item>
+                </Menu>
+            </Header>
             <Content className = 'dashboard-content'>
                 <AdminRoutes />
             </Content>
